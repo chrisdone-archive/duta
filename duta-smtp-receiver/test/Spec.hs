daec50c5e62f691544f71f258cc488704a4d3e71
Chris Done <chrisdone@gmail.com>
2018-07-27 17:45:10 +0100

    Fix mime parser

--- duta-smtp-receiver/test/Spec.hs
+++ duta-smtp-receiver/test/Spec.hs
@@ -9,6 +9,7 @@ module Main (main) where
 
 import           Codec.MIME.Base64
 import           Codec.MIME.Parse
+import qualified Codec.MIME.QuotedPrintable as QP
 import           Codec.MIME.Type
 import           Control.Monad.Logger
 import           Control.Monad.Reader
@@ -84,7 +85,39 @@ spec = do
           "GMail Attachment example"
           (shouldBe
              (parseMIMEMessage gmailAttachmentMessage)
-             gmailAttachmentParsed))
+             gmailAttachmentParsed)
+        it
+          "QuotedPrintable parser"
+          (shouldBe
+             (T.decodeUtf8
+                (S8.pack
+                   (QP.decode
+                      "/home/mlitchard/projects/git/reflex-todo/.stack-work/downloaded/Vheiln5kqwE=\n\
+                      \0/src/JSDOM/Custom/XMLHttpRequest.hs:39:46: error:\n\
+                      \    =E2=80=A2 Could not deduce (Control.Monad.Catch.MonadThrow\n\
+                      \                          Language.Javascript.JSaddle.Types.JSM)\n\
+                      \        arising from a use of =E2=80=98throwM=E2=80=99\n\
+                      \      from the context: MonadDOM m\n\
+                      \        bound by the type signature for:\n\
+                      \                   throwXHRError :: MonadDOM m =3D&gt; Maybe XHRError -&gt;=\n\
+                      \ m ()\n\
+                      \        at src/JSDOM/Custom/XMLHttpRequest.hs:38:1-53\n\
+                      \    =E2=80=A2 In the second argument of =E2=80=98(.)=E2=80=99, namely =E2=\n\
+                      \=80=98throwM=E2=80=99\n\
+                      \      In the second argument of =E2=80=98maybe=E2=80=99, namely =E2=80=98(l=\n\
+                      \iftDOM . throwM)=E2=80=99\n\
+                      \      In the expression: maybe (return ()) (liftDOM . throwM)")))
+             "/home/mlitchard/projects/git/reflex-todo/.stack-work/downloaded/Vheiln5kqwE0/src/JSDOM/Custom/XMLHttpRequest.hs:39:46: error:\n\
+                      \    • Could not deduce (Control.Monad.Catch.MonadThrow\n\
+                      \                          Language.Javascript.JSaddle.Types.JSM)\n\
+                      \        arising from a use of ‘throwM’\n\
+                      \      from the context: MonadDOM m\n\
+                      \        bound by the type signature for:\n\
+                      \                   throwXHRError :: MonadDOM m =&gt; Maybe XHRError -&gt; m ()\n\
+                      \        at src/JSDOM/Custom/XMLHttpRequest.hs:38:1-53\n\
+                      \    • In the second argument of ‘(.)’, namely ‘throwM’\n\
+                      \      In the second argument of ‘maybe’, namely ‘(liftDOM . throwM)’\n\
+                      \      In the expression: maybe (return ()) (liftDOM . throwM)"))
 
 writingToDb :: Spec
 writingToDb =
@@ -99,6 +132,9 @@ writingToDb =
                    , Duta.Types.Model.messageFrom = "Christopher Done <chrisdone@gmail.com>"
                    , Duta.Types.Model.messageTo = "wibble@chrisdone.com"
                    , Duta.Types.Model.messageSubject = "Re: wibbling"
+                   , Duta.Types.Model.messageParent = Nothing
+                   , Duta.Types.Model.messageThread = toSqlKey 1
+                   , Duta.Types.Model.messageIdentifier = Just "<CAAJHNPCnR2LVyN+Ns5TauNTC9Gb1hVnUHGD8+fKstAqm_5yvQA@mail.gmail.com>"
                    }
            (replies, inserted) <-
              runNoLoggingT
