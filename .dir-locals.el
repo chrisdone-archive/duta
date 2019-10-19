640c6e59e52c7ced1f8c924ceb6e545fa07d637d
Chris Done <chrisdone@gmail.com>
2019-10-19 22:32:48 +0100

    Remove unneeded files

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((haskell-mode
-  (intero-targets "duta-model:lib" "duta-smtp-receiver:lib" "duta-smtp-receiver:exe:duta-smtp-receiver" "duta-smtp-receiver:test:duta-smtp-receiver-test" "duta-smtp-receiver:bench:duta-smtp-receiver-bench" "duta-types:lib" "duta-web:lib" "duta-web:exe:duta-web" "spf:lib" "spf:exe:spf")))
